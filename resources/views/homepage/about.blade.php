f03c5c65e2af83393f007b9b75df0a088a775c7c
THEO <theofilus267@gmail.com>
2024-05-21 12:21:46 +0700

    y

---
+++ resources/views/homepage/about.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.template')
+@section('content')
+<div class="container1 d-flex justify-content-center align-items-center">
+  <div class="col">
+    <div class="d-flex flex-column align-items-center fadeIn">
+      <div class="judul-container">
+        <h2 class="judul">Tentang Kami</h2>
+      </div>
+      <div class="text-container">
+        <p>Selamat datang di WEOLAN. Kami adalah sebuah platform yang didedikasikan untuk memudahkan Anda dalam melakukan top up kebutuhan game.
+           Dengan menggunakan metode transaksi otomatis (Auto Transaction Method),
+           kami menyediakan layanan yang cepat, aman, dan dapat diandalkan, sehingga Anda dapat menikmati pengalaman bermain game tanpa gangguan.</p>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
+
+<style>
+.container1 {
+  height: 50vh;
+  border-radius: 30px;
+  overflow: hidden;
+  position: relative;
+  margin: 20px;
+  background-image: url('/images/BG1.jpeg');
+  background-size: cover;
+  background-position: top;
+  background-repeat: no-repeat;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.judul-container {
+  position: absolute;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  background-color: rgba(0, 0, 0, 0.7);
+  padding: 10px 20px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);
+  text-align: center;
+}
+
+.judul {
+  color: white;
+  margin: 0;
+}
+
+.text-container {
+  background-color: rgba(0, 0, 0, 0.7);
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.5);
+  text-align: center;
+  color: white;
+  margin-top: 60px;
+}
+
+.fadeIn {
+  animation: fadeIn 1s ease-in-out;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
---
+++ resources/views/homepage/produk.blade.php
@@ -0,0 +1,118 @@
+@extends('layouts.template')
+@section('content')
+<div class="container" style="margin-bottom:70px;">
+
+
+
+  <!-- kategori produk -->
+<<<<<<< HEAD
+  <div class="card" style="padding: 20px; background-color: #ADC178">
+    <div class="bg-transparent" >
+      <h2 class="text-left" style="color: white; font-weight:bold; margin-bottom: 20px;">Product Category</h2>
+      <div class="btn-group d-flex flex-wrap shadow-none mt-1 mt-lg-1 mt-md-1 mt-xl-1 ms-2 ms-lg-2 ms-md-2 ms-xl-2">
+      @foreach($listkategori as $kategori)
+        <a style="width: 150px; font-size: 13px; font-weight:bold; font-family: 'Poppins' sans-serif;" href="{{ URL::to('category/'.$kategori->slug_kategori) }}" class="btn mt-1 mt-lg-1 mt-md-1 mt-xl-1 mx-2 mx-lg-2 mx-md-2 mx-xl-2 rounded">
+=======
+  <div class="card" style="padding: 20px; background-image:linear-gradient(#4139d4, #a0adeb);">
+    <div class="bg-transparent" >
+      <h2 class="text-left" style="font-weight:bold; margin-bottom: 20px; color: white">Product Category</h2>
+      <div class="btn-group d-flex flex-wrap shadow-none mt-1 mt-lg-1 mt-md-1 mt-xl-1 ms-2 ms-lg-2 ms-md-2 ms-xl-2" stylw="">
+      @foreach($listkategori as $kategori)
+        <a style="width: 150px; font-size: 13px; font-weight:bold; font-family: 'Poppins' sans-serif; color:white; background-color:#a0adeb" href="{{ URL::to('category/'.$kategori->slug_kategori) }}" class="btn mt-1 mt-lg-1 mt-md-1 mt-xl-1 mx-2 mx-lg-2 mx-md-2 mx-xl-2 rounded">
+>>>>>>> 2e54129a (gabung2 in file)
+          {{ $kategori->nama_kategori }}</span>
+        </a>
+      @endforeach
+      </div>
+    </div>
+  </div>
+  <!-- end kategori produk -->
+  <form action="" class="" name="sortProducts" id="sortProducts">
+<<<<<<< HEAD
+  <div class="toolbar-sorted">
+=======
+  <div class="toolbar-sorted" style="margin:20px">
+>>>>>>> 2e54129a (gabung2 in file)
+    <div class="select-box-wrapper">
+      <label for="sort-by" class="sr-only">Sort By</label>
+      <select name="sort" class="select-box" id="sort-by">
+        <option value="/product" selected="selected">Sort by : Latest Product</option>
+        <option value="price_low">Sort by : Lowets Price</option>
+        <option value="price_high">Sort by : Highest Price</option>
+        <option value="products_asc">Sort by : Name A-Z</option>
+        <option value="products_desc">Sort by : Name Z-A</option>
+      </select>
+    </div>
+  </div>
+  </form>
+
+    <!-- <form action="/product" method="GET">
+      <div class="input-group">
+        <input type="search" class="form-control rounded" name="price" autocomplete="off" placeholder="Price" aria-label="Search" aria-describedby="search-addon" />
+        <button type="submit" class="btn btn-outline-primary">search</button>
+      </div>
+    </form> -->
+
+  <!-- produk Terbaru-->
+  <div class="row mt-4" style="margin-top: 30px; margin-bottom: 30px;">
+    <div class="col col-md-12 col-sm-12 mb-4">
+<<<<<<< HEAD
+      <h2 class="text-left" style="font-weight:bold;">Terbaru</h2>
+=======
+      <h2 class="text-left" style="font-weight:bold; color:white">Terbaru</h2>
+>>>>>>> 2e54129a (gabung2 in file)
+    </div>
+    @foreach($itemproduk as $produk)
+    <!-- produk pertama -->
+    <div class="col-md-4">
+      <div class="card mb-4">
+      <div style="height: 190px; max-width: 270px; display: flex; align-items: center; margin-left: auto; margin-right: auto;">
+        <a href="{{ URL::to('product/'.$produk->slug_produk ) }}">
+          @if($produk->foto != null)
+          <img src="{{ \Storage::url($produk->foto) }}" alt="{{ $produk->nama_produk }}" class="card-img-top" style="max-height: 190px; width: 100%;">
+          @else
+          <img src="{{ asset('images/bag.jpg') }}" alt="{{ $produk->nama_produk }}" class="card-img-top" style="max-height: 190px; width: 100%;">
+          @endif
+        </a>
+      </div>
+<<<<<<< HEAD
+        <div class="card-body" style="border:none; background-color: #ADC178;">
+=======
+        <div class="card-body" style="border:none; background-image:linear-gradient(#4139d4, #a0adeb);">
+>>>>>>> 2e54129a (gabung2 in file)
+        <div class="row mt-4">
+        <div class="col">  
+            <a class="text-decoration-none" style="color: black;">
+            <p class="card-text h5">
+              <strong>{{ $produk->nama_produk }}</strong>
+            </p>
+          </a>
+          </div>
+          <div class="col-auto">
+              <p>
+                Rp. {{ number_format($produk->harga, 2) }}
+              </p>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col">
+<<<<<<< HEAD
+              <a class="btn" href="{{ URL::to('product/'.$produk->slug_produk ) }}">
+=======
+              <a class="btn" style="background-color:white"href="{{ URL::to('product/'.$produk->slug_produk ) }}">
+>>>>>>> 2e54129a (gabung2 in file)
+                Detail
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    @endforeach
+</div>
+</div>
+<<<<<<< HEAD
+@endsection
+=======
+@endsection
+>>>>>>> 2e54129a (gabung2 in file)
